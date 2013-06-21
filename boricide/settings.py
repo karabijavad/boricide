7e663ec06490b5b196f985af2d33fa72fef6d7ad
Javad Karabi <jkarabi@caxy.com>
2013-06-21 00:08:27 -0500

    begin using tastypie

--- boricide/settings.py
+++ boricide/settings.py
@@ -121,7 +121,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'boricide'
+    'boricide',
+    'tastypie',
 )
 
 # A sample logging configuration. The only tangible logging
--- boricide/urls.py
+++ boricide/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from boricide.models import Artist, Venue, Concert
+from tastypie.api import Api
+
+v1_api = Api(api_name='v1')
 
 admin.autodiscover()
 admin.site.register(Artist)
