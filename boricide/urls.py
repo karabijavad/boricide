995d611dcbf65dd57a622a431ca72373bea6cefc
Javad Karabi <jkarabi@caxy.com>
2013-06-20 23:57:20 -0500

    register Artist, Venue, Concert models with admin interface.

--- boricide/urls.py
+++ boricide/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from boricide.models import Artist, Venue, Concert
 
 admin.autodiscover()
+admin.site.register(Artist)
+admin.site.register(Venue)
+admin.site.register(Concert)
 
 urlpatterns = patterns('',
     (r'^admin/',  include(admin.site.urls)),
