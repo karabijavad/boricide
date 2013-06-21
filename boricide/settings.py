396cffdf3fea590fe961ed52a8ca4ef797744794
Javad Karabi <jkarabi@caxy.com>
2013-06-20 23:17:50 -0500

    sets STATIC_ROOT to (and gitignores) project_dir/static.

--- boricide/settings.py
+++ boricide/settings.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 # Django settings for boricide project.
 
 DEBUG = True
@@ -60,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, 'static'))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
