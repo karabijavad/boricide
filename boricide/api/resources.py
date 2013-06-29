5541c96af4eba3de9e216a88188f35fe53140e72
Javad Karabi <jkarabi@caxy.com>
2013-06-28 23:11:52 -0500

    cleanup

--- boricide/api/resources.py
+++ boricide/api/resources.py
@@ -1,20 +1,24 @@
-from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
-from tastypie import fields, utils
+from tastypie.resources import ModelResource, ALL
+from tastypie import fields
 from boricide.models import Event, Artist, Venue, Concert
 from tastypie.authorization import DjangoAuthorization
 from tastypie.authentication import ApiKeyAuthentication
 
+
 class EventResource(ModelResource):
     def dehydrate_start_time(self, bundle):
         return bundle.obj.start_time.isoformat()
+
     def dehydrate_end_time(self, bundle):
         return bundle.obj.end_time.isoformat()
+
     class Meta:
         authentication = ApiKeyAuthentication()
         authorization = DjangoAuthorization()
         queryset = Event.objects.all()
         resource_name = 'event'
         always_return_data = True
+
 
 class ArtistResource(ModelResource):
     class Meta:
@@ -24,6 +28,7 @@ class ArtistResource(ModelResource):
         resource_name = 'artist'
         always_return_data = True
 
+
 class VenueResource(ModelResource):
     class Meta:
         authentication = ApiKeyAuthentication()
@@ -32,9 +37,11 @@ class VenueResource(ModelResource):
         resource_name = 'venue'
         always_return_data = True
 
+
 class ConcertResource(EventResource):
     artists = fields.ToManyField(ArtistResource, 'artists', full=True)
-    venue   = fields.ToOneField(VenueResource, 'venue', full=True)
+    venue = fields.ToOneField(VenueResource, 'venue', full=True)
+
     class Meta:
         authentication = ApiKeyAuthentication()
         authorization = DjangoAuthorization()
--- boricide/models.py
+++ boricide/models.py
@@ -1,25 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tastypie.models import create_api_key
-import urllib2, urllib
+import urllib2
+import urllib
 import json
 
 models.signals.post_save.connect(create_api_key, sender=User)
 
+
 class Artist(models.Model):
-    name        = models.CharField(max_length=100)
-    website     = models.CharField(max_length=100, blank=True)
+    name = models.CharField(max_length=100)
+    website = models.CharField(max_length=100, blank=True)
     description = models.TextField(blank=True)
 
     def __unicode__(self):
         return self.name
 
+
 class Venue(models.Model):
-    name        = models.CharField(max_length=100)
-    address     = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    lat         = models.DecimalField(max_digits=20, decimal_places=10, blank=True)
-    lng         = models.DecimalField(max_digits=20, decimal_places=10, blank=True)
+    lat = models.DecimalField(max_digits=20, decimal_places=10, blank=True)
+    lng = models.DecimalField(max_digits=20, decimal_places=10, blank=True)
 
     def save(self, *args, **kwargs):
         url = 'http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=' + urllib.quote_plus(self.address)
@@ -36,17 +39,19 @@ class Venue(models.Model):
     def __unicode__(self):
         return self.name
 
+
 class Event(models.Model):
-    name            = models.CharField(max_length=100)
-    start_time      = models.DateTimeField()
-    end_time        = models.DateTimeField()
-    venue           = models.ForeignKey(Venue)
-    door_price      = models.DecimalField(max_digits=5, decimal_places=2)
-    advance_price   = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
-    description     = models.TextField(blank=True)
+    name = models.CharField(max_length=100)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    venue = models.ForeignKey(Venue)
+    door_price = models.DecimalField(max_digits=5, decimal_places=2)
+    advance_price = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    description = models.TextField(blank=True)
 
     def __unicode__(self):
         return self.name
+
 
 class Concert(Event):
     artists = models.ManyToManyField(Artist)
