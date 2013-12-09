a54615922f256f6067d31c57f8079b63fe7ec807
Javad Karabi <karabijavad@gmail.com>
2013-12-09 05:33:52 +0000

    ticketfly

--- boricide/management/commands/ticketfly.py
+++ boricide/management/commands/ticketfly.py
@@ -3,71 +3,74 @@ from django.db.utils import IntegrityError
 import urllib2
 import json
 from boricide.models import *
-import datetime
 import dateutil
 from pytz import timezone
-import pytz
-import sys, traceback
-import sys, os
+import sys
+import traceback
 import re
+
 
 class Command(BaseCommand):
     pageNum = 0
     maxResults = 200
     data_url = "http://www.ticketfly.com/api/events/upcoming.json?orgId=1"
-    
+
     def handle(self, *args, **options):
-      while True:
-        print "on page " + str(self.pageNum)
-        this_page = self.data_url + "&pageNum=" + str(self.pageNum) + "&maxResults=" + str(self.maxResults)
-        data = json.loads(urllib2.urlopen(this_page).read())
-        if self.pageNum > data["totalPages"]:
-          print "hit last page, stopping"
-          break
+        while True:
+            print "on page " + str(self.pageNum)
+            this_page = self.data_url + "&pageNum=" + \
+                str(self.pageNum) + "&maxResults=" + str(self.maxResults)
+            data = json.loads(urllib2.urlopen(this_page).read())
+            if self.pageNum > data["totalPages"]:
+                print "hit last page, stopping"
+                break
 
-        for event in data["events"]:
-          if not event["endDate"]:
-            endDate = timezone(event["org"]["timeZone"]).localize(dateutil.parser.parse(event["startDate"]))
-            print "No end date, skipping show: " + event["name"]
-            continue
-          if event["ticketPrice"] == "FREE" or "No cover":
-            event["ticketPrice"] = "0"
+            for event in data["events"]:
+                if not event["endDate"]:
+                    endDate = timezone(event["org"]["timeZone"]).localize(
+                        dateutil.parser.parse(event["startDate"]))
+                    print "No end date, skipping show: " + event["name"]
+                    continue
+                if event["ticketPrice"] == "FREE" or "No cover":
+                    event["ticketPrice"] = "0"
 
-          try:
-              this_venue,created       = Venue.objects.get_or_create(
-                name = event["venue"]["name"],
-                defaults = {
-                  "address": event["venue"]["address1"] + ", " + event["venue"]["city"] + ", " + event["venue"]["stateProvince"]
-                }
-              )
-          except Exception as e:
-            print "Exception: " + str(e)
-            continue
+                try:
+                    this_venue, created = Venue.objects.get_or_create(
+                        name=event["venue"]["name"],
+                        defaults={
+                            "address": event["venue"]["address1"] + ", " + event["venue"]["city"] + ", " + event["venue"]["stateProvince"]
+                        }
+                    )
+                except Exception as e:
+                    print "Exception: " + str(e)
+                    continue
 
-          if created:
-            print "Created new venue: " + event["venue"]["name"] + ", " + event["venue"]["address1"] + ", " + event["venue"]["city"] + ", " + event["venue"]["stateProvince"]
+                if created:
+                    print "Created new venue: " + event["venue"]["name"] + ", " + event["venue"]["address1"] + ", " + event["venue"]["city"] + ", " + event["venue"]["stateProvince"]
 
-          try:
-            this_concert = Concert.objects.get_or_create(
-              name        = event["name"],
-              start_time  = timezone(event["org"]["timeZone"]).localize(dateutil.parser.parse(event["startDate"])),
-              end_time    = timezone(event["org"]["timeZone"]).localize(dateutil.parser.parse(event["endDate"])),
-              door_price  = re.sub('-.*' ,'', event["ticketPrice"].replace("$","")),
-              venue       = this_venue
-            )[0]
-          except:
-            traceback.print_exc(file=sys.stdout)
-            continue
-            #sys.exit()
+                try:
+                    this_concert = Concert.objects.get_or_create(
+                        name=event["name"],
+                        start_time=timezone(event["org"]["timeZone"]).localize(
+                            dateutil.parser.parse(event["startDate"])),
+                        end_time=timezone(event["org"]["timeZone"]).localize(
+                            dateutil.parser.parse(event["endDate"])),
+                        door_price=re.sub(
+                            '-.*', '', event["ticketPrice"].replace("$", "")),
+                        venue=this_venue
+                    )[0]
+                except:
+                    traceback.print_exc(file=sys.stdout)
+                    continue
+                    # sys.exit()
 
-          for artist in event["headliners"]:
-            this_concert.artists.add(
-              Artist.objects.get_or_create(
-                name = artist["name"],
-                defaults = {
-                  "description": artist.get("description", "")
-                }
-              )[0]
-            )
-        self.pageNum += 1
-
+                for artist in event["headliners"]:
+                    this_concert.artists.add(
+                        Artist.objects.get_or_create(
+                            name=artist["name"],
+                            defaults={
+                                "description": artist.get("description", "")
+                            }
+                        )[0]
+                    )
+            self.pageNum += 1
