0592e117cb29c23e704dd579e2efb2cf20bfa920
jsn <jasonhfields@gmail.com>
2013-12-15 21:49:29 +0000

    clean

--- boricide/scrape/emptybottle.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from boricide.models import Event, Concert, Artist, Venue
-import datetime
-import mechanize
-import json
-
-def scrape(request):
-  return HttpResponse("")
